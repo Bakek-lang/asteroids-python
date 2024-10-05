10d3b614a44b00bf882ede459acc54f19620b15b
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-10-05 20:36:08 +0200

    collision detection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -35,6 +36,12 @@ def main():
 
         for obj in updatable:
             obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
+
 
         screen.fill("black")
 
